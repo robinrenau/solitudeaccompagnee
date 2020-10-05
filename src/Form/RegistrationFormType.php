ec7e4cc23f10415267daf15138d9db8bc60ba038
robinrenau <robinrenau@gmail.com>
2020-10-05 14:51:59 +0200

    customization of registration and login forms

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -25,6 +25,7 @@ class RegistrationFormType extends AbstractType
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
+                'label' => 'Mot de passe',
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
