5dcbeb2278efd6868253a0b86b67b4b920d77700
robinrenau <robinrenau@gmail.com>
2020-10-13 18:21:04 +0200

    responsive optimization and resolution of some end-of-day bugs

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -122,7 +122,7 @@ class ResetPasswordController extends AbstractController
             // The session is cleaned up after the password has been changed.
             $this->cleanSessionAfterReset();
 
-            return $this->redirectToRoute('user_show', ['id' => $user->getId()]);
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('reset_password/reset.html.twig', [
