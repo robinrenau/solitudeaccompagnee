5cdd7d36fc1f077f816ca5b1cacd3a6682083dbe
robinrenau <robinrenau@gmail.com>
2020-10-13 10:53:15 +0200

    translation of the reset password part + creation of the button in the user page

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -30,23 +30,7 @@ class UserType extends AbstractType
 
             ])
 
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'label' =>'Mot de passe',
-                'mapped' => false,
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
-            ])
+
             ->add('profilPicture', FileType::class, [
                 'mapped' => false,
                 'required'=>false,
