c4c1c24c9a4d3df9cff580c692859d78d1f76a14
robinrenau <robinrenau@gmail.com>
2020-10-19 17:19:40 +0200

    Refactoring of my js code, from base.html.twig to app.js (for my participations)

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,20 +9,47 @@
 import '../css/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
-
-import 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
-import 'mdbootstrap';
 import axios from 'axios';
-
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
-
-// Code des selects formulaires
-
-
-
-// code du datepicker (à modifier)
 
 
 // code pour l'ajax participation
+document.querySelectorAll('a.js-participation').forEach(function(link){
+    function OnClickBtnParticipation(event){
+        event.preventDefault();
+        const url = this.href;
+        const spanCount2 = document.querySelector('span#js-participations');
+        const icone = this.querySelector('i');
+
+        axios.get(url).then(function(response){
+
+            spanCount2.textContent = response.data.participations;
+
+            if(icone.classList.contains('fa-times-circle'))
+                icone.classList.replace('fa-times-circle','fa-check-circle');
+            else
+                icone.classList.replace('fa-check-circle','fa-times-circle' )
+        }).catch(function (error) {
+            if (error.response.status=== 403){
+                window.alert("Vous ne pouvez pas participer à un évenement si vous n'êtes pas connecté")
+            } else{
+                window.alert("Une erreur s'est produite, veuillez réessayer plus tard")
+            }
+
+        });
+    }
+    link.addEventListener('click', OnClickBtnParticipation)
+});
+
+
+// Code upload photo profil
+$('.dropdown-toggle').dropdown();
+$('.custom-file-input').on('change', function(event) {
+    var inputFile = event.currentTarget;
+    $(inputFile).parent()
+        .find('.custom-file-label')
+        .html(inputFile.files[0].name);
+});
+
+
